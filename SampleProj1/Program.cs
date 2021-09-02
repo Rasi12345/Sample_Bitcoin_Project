9fed43c908401b2826817f67203364a0458b3fae
mehtar3 <rasika.mehta3@mail.dcu.ie>
2021-09-03 00:17:31 +0530

    Add project files.

---
+++ SampleProj1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+using (WebClient wc = new WebClient())
+{
+    var json = wc.DownloadString("https://api.coindesk.com/v1/bpi/currentprice.json");
+
+    Console.WriteLine(json);
+
+    JObject parsed = JObject.Parse(json);
+
+    Console.WriteLine("Current rate of " + Convert.ToString(parsed["chartName"]) + " in " + Convert.ToString(parsed["bpi"]["EUR"]["code"]) + " is " + Convert.ToString(parsed["bpi"]["EUR"]["rate"]));
+}
+
+
